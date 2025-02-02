153e0c4a05d84274b3ae5d068393b4105c377eba
Hadi_jawdi <hadi.jawdi2006@gmail.com>
2025-02-02 15:58:42 +0330

    Coded and explained strings in Memory

--- fourth_week.c
+++ fourth_week.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
 #include "cs50.h"
 
 
 int main(void)
 { 
     
+    //  
+    string user_name = get_string("UserName: ");
+    printf("%s\n",user_name);
+    // print the location of this string in the meomory
+    printf("%p\n", user_name);
+    // printf the location of it's charctes 
+    // & give me the location of .. 
 
+    printf("%p\n",&user_name[0]);
+    printf("%p\n",&user_name[1]);
 }
 
 
